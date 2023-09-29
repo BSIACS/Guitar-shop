359d73c1e20114520c1e8cc5754cbbf7b5d0924e
BSI <bsi.acs14@gmail.com>
2023-09-29 21:42:53 +0300

    Добавлен функционал команды генерации данных БД.

--- apps/cli/mock/mock-data.ts
+++ apps/cli/mock/mock-data.ts
@@ -1,11 +1,41 @@
-export const guitarManufacturer = ['Esp', 'Schecter', 'Gibson', 'Fender', 'Cort'];
+export const titles = ['Esp', 'Schecter', 'Gibson', 'Fender', 'Cort', 'Yerasov', 'Shamray', 'Ltd', 'Jackson'];
 
-export const productDescription = [
+export const description = [
   'Inspired by the Abstract Limited Edition that was introduced in 2018, the guitar was developed in response to the growing demand for a follow-up model. New design elements were developed using combinations of triangular shapes that form geometrical designs on the headstock and fretboard position markers.',
   'Guitar is designed to be a very accessible acoustic guitar which is based on the popular Grand Auditorium body shape that boasts a full yet balanced sound. Great for everyday usage, and thanks to its integrated preamp system, the guitar is also suitable for live performances.',
   'If you’re ready to rock but want a guitar that’s as easy on your budget as it is powerful in the studio and on stage, the guitar is a great choice. Built with set-neck construction at 24.75” scale, the guitar has a mahogany body and comfortable three-piece maple neck that features a roasted jatoba fingerboard with 24 extra-jumbo frets and dot inlays.',
   'Guitars are designed to offer the tone, feel, looks, and quality that working professional musicians need in an instrument. The guitar is a seven-string guitar that offers high-end playability along with the extended range of an additional string to broaden your tonal horizons. It also includes high-end components like LTD locking tuners and a Hipshot bridge with string-thru-body design, as well as Seymour Duncan pickups that are direct-mounted.',
   'Transducer-electric guitar series designed for musicians who want the sound of an acoustic guitar, but with the playing comfort and fast feel of an electric guitar. The remarkable guitar is the first 7-string model in this series. It gives you the expanded range you need for specialized music forms, with the lowest string typically tuned to B. The guitar has high-quality components like a Graphtech Nubone XB nut and saddle, Grover tuners, a Fishman S-Core pickup, and Fishman TL-3 preamp with onboard tuner.'
+];
+
+export const typeId = [
+  '12a664cb-c8b9-4e44-817b-fa220fb48cf8',
+  '20670fbe-c926-48e9-ae7e-b8d6b09740dd',
+  'be66a279-8e7c-497b-8b29-b3107c2387e7'
+];
+
+export const stringNumber = [4, 6, 7, 12];
+
+export const imgSrc = [
+  'catalog-product-1.png',
+  'catalog-product-2.png',
+  'catalog-product-3.png',
+  'catalog-product-4.png',
+  'catalog-product-5.png',
+  'catalog-product-6.png',
+  'catalog-product-7.png',
+  'catalog-product-8.png',
+];
+
+export const article = [
+  'QW826542',
+  'ZV937166',
+  'MQ185304',
+  'QW109922',
+  'FD984214',
+  'MK905321',
+  'MQ183426',
+  'ZX352845',
 ];
 
 
--- apps/cli/src/app/commands/generate-command.ts
+++ apps/cli/src/app/commands/generate-command.ts
@@ -1,24 +1,97 @@
-
+import { genSalt, hash } from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 import { CliCommandInterface } from './cli-command.interface.js';
+import { v4 as uuidv4 } from 'uuid';
+import { article, description, imgSrc, stringNumber, titles, typeId } from '../../../mock/mock-data';
+import { generateRandomValue, getRandomDate, getRandomItem } from '../utils/random'
 
 type Client = typeof PrismaClient.prototype;
+
+const SALT_ROUNDS = 10;
+const DEFAULT_USER_NAME = 'admin';
+const DEFAULT_USER_EMAIL = 'admin@somemail.com';
+const DEFAULT_USER_PASSWORD = 'admin';
+
+const MIN_PRODUCT_PRICE = 100;
+const MAX_PRODUCT_PRICE = 1000000;
+
 
 export default class GenerateCommand implements CliCommandInterface {
   public readonly name;
   public readonly prismaClient: Client;
 
-  constructor(){
+  constructor() {
     this.name = '--generate';
     this.prismaClient = new PrismaClient();
   }
 
-  public async execute(...parameters: string[]): Promise<void>{
-    const [quantity, filepath, url] = parameters;
+  public async execute(...parameters: string[]): Promise<void> {
+    const [quantity] = parameters;
     const productsQuantity = Number.parseInt(quantity, 10);
+
+    await this.createUser();
+    await this.createTypes();
+    await this.createProducts(productsQuantity);
   }
 
-  public async generateProducts(quantity: number){
-    console.info('generateProducts function not implemented');
+  public async createProducts(quantity: number) {
+    const products = [];
+
+    for (let i = 0; i < quantity; i++) {
+      products.push({
+        id: uuidv4(),
+        title: getRandomItem(titles),
+        description: getRandomItem(description),
+        dateAdded: getRandomDate(new Date("2017-01-26"), new Date()),
+        imageSrc: getRandomItem(imgSrc),
+        typeId: getRandomItem(typeId),
+        article: getRandomItem(article),
+        stringNumber: getRandomItem(stringNumber),
+        price: generateRandomValue(MIN_PRODUCT_PRICE, MAX_PRODUCT_PRICE).toString()
+      });
+    }
+
+    await this.prismaClient.product.createMany({
+      data: products
+    });
+  }
+
+  public async createUser() {
+    const salt = await genSalt(SALT_ROUNDS);
+
+    await this.prismaClient.user.upsert({
+      where: {
+        email: DEFAULT_USER_EMAIL,
+      },
+      update: {
+
+      },
+      create: {
+        id: uuidv4(),
+        name: DEFAULT_USER_NAME,
+        email: DEFAULT_USER_EMAIL,
+        passwordHash: await hash(DEFAULT_USER_PASSWORD, salt),
+      },
+
+    });
+  }
+
+  public async createTypes() {
+    await this.prismaClient.type.createMany({
+      data: [
+        {
+          id: '12a664cb-c8b9-4e44-817b-fa220fb48cf8',
+          name: 'electric'
+        },
+        {
+          id: '20670fbe-c926-48e9-ae7e-b8d6b09740dd',
+          name: 'acoustic'
+        },
+        {
+          id: 'be66a279-8e7c-497b-8b29-b3107c2387e7',
+          name: 'ukulele'
+        }
+      ],
+    });
   }
 }
